9765b9cabcbc7c8026c2471eb57f4c8e9b4cffe0
Grycek <adam.grycner@wp.pl>
2012-03-09 02:43:28 -0800

    Redir

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -8,6 +8,9 @@ class MoviesController < ApplicationController
 
   def index
     @all_ratings = Movie.all_ratings
+    r = Random.new
+    redirect = r.rand(0..4) > 0
+    
     [:checked, :ratings, :sort].each do |i|
       params[i] = session[i] if params[i] == nil
       session[i] = params[i] if params[i] != nil
@@ -17,6 +20,7 @@ class MoviesController < ApplicationController
     #
     rates = []
     rates = params[:ratings].keys unless params[:ratings] == nil
+    #
     @movies = Movie.find_all_by_rating( rates )# if params[:sort] == nil
     @movies = Movie.find_all_by_rating( rates, :order => 'title') if params[:sort] == 'title'
     @movies = Movie.find_all_by_rating( rates, :order => 'release_date') if params[:sort] == 'date'
@@ -24,6 +28,8 @@ class MoviesController < ApplicationController
     params[:title_css] = params[:date_css] = 'None'
     params[:title_css] = 'hilite' if params[:sort] == 'title'
     params[:date_css]  = 'hilite' if params[:sort] == 'date'
+    redirect_to :action => 'index', :sort => params[:sort], :ratings => params[:ratings] if redirect
+    #render :action => 'index', :params => params
   end
 
   def new
